75f0ebff5ddd651eec95ecd991ec059f4b28ea44
smccrav1 <smccrav1@ggc.edu>
2015-03-23 23:41:07 +0000

    changed image, added credit page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'home/about'
 
-  get 'home/credits'
+  get 'credits' => 'home#credits'
 
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
